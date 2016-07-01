8b1b9b9a97cca082d2c458d9a37edcab5cb1a9e8
Tom Hunkapiller <tom@furycodes.com>
2016-07-01 18:49:52 -0500

    auto-populate commands

--- TheAwesomeBot.js
+++ TheAwesomeBot.js
@@ -4,77 +4,45 @@ var bot = new Discord.Client();
 
 var helpCommands = require('./commands/helpText.js');
 
-var interpolate = require('interpolate');
+// var interpolate = require('interpolate');
 
-var result = interpolate('{greeting}! I am {name}.', {
-  greeting: 'Hello',
-  name: 'TheAwesomeBot',
+// var result = interpolate('{greeting}! I am {name}.', {
+//   greeting: 'Hello',
+//   name: 'TheAwesomeBot',
+// });
+// console.log(result); // 'Hello! I am TheAwesomeBot.'
+
+// also, in es2015 (node >= 4.x)
+// var data = {
+//   greeting: 'Hello',
+//   name: 'TheAwesomeBot',
+// };
+// var result = `${data.greeting}! I am ${data.name}.`;
+// console.log(result); // 'Hello! I am TheAwesomeBot.'
+
+var botcmd = '!bot',
+  helpcmd = 'help';
+
+var basicHelp = "Awesome is my name, don't wear it out! " + 
+  "Please type '!bot help *channel you need help with*' for more info.";
+
+var helpObj = {};
+helpObj[botcmd + ' ' + helpcmd] = helpObj[botcmd] = basicHelp
+
+
+var simpleResponses = Object.assign(helpObj,
+  Object.keys(helpCommands)
+    .map(k => [`${botcmd} ${helpcmd} ${k}`, helpCommands[k]])
+    .reduce( (obj, row) => (obj[row[0]] = row[1], obj), {}))
+
+console.log(simpleResponses)
+
+
+
+bot.on('message', function (message) {
+  var key = message.toLowerCase().trim()
+  if (simpleResponses[key]) return bot.reply(message, simpleResponses[key])
+
 });
-
-console.log(result); // 'Hello! I am TheAwesomeBot.'
-
-bot.on('message', function (message)
-{
-
-    switch (message.content)
-    {
-
-    case '!bot help':
-      {
-        bot.reply(message, "Awesome is my name, don't wear it out! Please type '!bot help *channel you need help with*' for more info.");
-        break;
-      }
-
-    case '!bot help python':
-      {
-        bot.reply(message, helpCommands.pythonText);
-        break;
-      }
-
-    case '!bot help java':
-      {
-        bot.reply(message, helpCommands.javaText);
-        break;
-      }
-
-    case '!bot help c_cpp':
-      {
-        bot.reply(message, helpCommands.cppText);
-        break;
-      }
-
-    case '!bot help php':
-      {
-        bot.reply(message, helpCommands.phpText);
-        break;
-      }
-
-    case '!bot help style':
-      {
-        bot.reply(message, "Here is a link to Discord's text styles: https://support.discordapp.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-");
-        break;
-      }
-
-    case '!bot':
-      {
-        bot.reply(message, "Awesome is my name, don't wear it out! Please type '!bot help *channel you need help with*' for more info.");
-        break;
-      }
-
-    case '!bot help javascript':
-      {
-        bot.reply(message, helpCommands.jsText);
-        break;
-      }
-
-    case '!bot help dotnet':
-    case '!bot help .net':
-    case '!bot help c#':
-      {
-        bot.reply(message, helpCommands.dnText);
-        break;
-      }
-  }
-  });
 
 bot.loginWithToken('MTk4MjQ5NTI1ODU1NTE4NzIx.Cldcvw.xAQgYTI9IN_ACmCTBVydTQiM66k');
--- commands/helpText.js
+++ commands/helpText.js
@@ -1,31 +1,37 @@
 module.exports = {
-  pythonText: 'See these links/courses for learning Python: \
+  python: 'See these links/courses for learning Python: \
 https://docs.python.org/2/ \
 https://docs.python.org/3/ \
 https://www.udacity.com/course/programming-foundations-with-python--ud036 \
 http://learnpythonthehardway.org/book/ \
 ',
-  javaText: 'See these links/courses for learning Java: \
+  java: 'See these links/courses for learning Java: \
 https://docs.oracle.com/javase/7/docs/api/ \
 https://docs.oracle.com/javase/8/docs/api/ \
 https://docs.oracle.com/javase/tutorial/ \
 ',
-  cppText: 'See these links/courses for learning C++: \
+  c_cpp: 'See these links/courses for learning C++: \
 http://www.cplusplus.com/doc/tutorial/ \
 ',
-  jsText: 'See these links/courses for learning JavaScript: \
+  javascript: 'See these links/courses for learning JavaScript: \
 https://www.codecademy.com/learn/javascript \
 https://www.udacity.com/course/javascript-basics--ud804 \
 https://developer.mozilla.org/en-US/docs/Web/JavaScript \
 ',
-  phpText: 'See these links/courses for learning PHP: \
+  php: 'See these links/courses for learning PHP: \
   http://www.w3schools.com/php/ \
   http://php.net/manual/en/tutorial.php \
   https://www.codecademy.com/learn/php \
   ',
-  dnText: 'See these links/courses for learning .NET/C#: \
+  dotnet: 'See these links/courses for learning .NET/C#: \
 https://msdn.microsoft.com/en-us/library/aa288436(v=vs.71).aspx \
 https://msdn.microsoft.com/en-us/library/xk24xdbe(v=vs.90).aspx \
 https://www.youtube.com/watch?v=Yj0G5UdBJZw&list=PLEbaEyM-xt9mVQEAXGlRRmbO2Qp_oqF-n \
 ',
+  style: "Here is a link to Discord's text styles: " +
+    "https://support.discordapp.com" +
+    "/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-"
 };
+
+module.exports['.net'] = module.exports.dotnet
+module.exports['c#'] = module.exports.dotnet
