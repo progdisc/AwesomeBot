98780c1c4cd2ab225f6a0fd4500e7e3afafecab4
fzxt <fahadahmad41@hotmail.com>
2016-07-01 14:49:21 -0400

    added helpText commands

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # commandText
+
+suggested:
+
+create different js files for different commands. group commands should reside in seperate modules.
---
+++ helpText.js
@@ -0,0 +1,21 @@
+module.exports = {
+  pythonText: 'See these links/courses for learning Python: \ \
+https://docs.python.org/2/ \ \
+https://docs.python.org/3/ \ \
+https://www.udacity.com/course/programming-foundations-with-python--ud036 \ \
+http://learnpythonthehardway.org/book/ \ \
+',
+  javaText: 'See these links/courses for learning Java: \
+https://docs.oracle.com/javase/7/docs/api/ \
+https://docs.oracle.com/javase/8/docs/api/ \
+https://docs.oracle.com/javase/tutorial/ \
+',
+  cppText: 'See these links/courses for learning C++: \
+http://www.cplusplus.com/doc/tutorial/ \
+',
+  jsText: 'See these links/courses for learning JavaScript: \
+https://www.codecademy.com/learn/javascript \
+https://www.udacity.com/course/javascript-basics--ud804 \
+https://developer.mozilla.org/en-US/docs/Web/JavaScript \
+',
+};
