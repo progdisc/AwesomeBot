b6ed8c57bfccfbf343295922d0de24df5df0e41f
Tom Hunkapiller <tom@furycodes.com>
2016-07-01 22:26:08 -0500

    add jseval

--- TheAwesomeBot.js
+++ TheAwesomeBot.js
@@ -4,15 +4,9 @@ var bot = new Discord.Client();
 
 var helpCommands = require('./commands/helpText.js');
 
-// var interpolate = require('interpolate');
+var vm = require('vm');
 
-// var result = interpolate('{greeting}! I am {name}.', {
-//   greeting: 'Hello',
-//   name: 'TheAwesomeBot',
-// });
-// console.log(result); // 'Hello! I am TheAwesomeBot.'
 
-// also, in es2015 (node >= 4.x)
 // var data = {
 //   greeting: 'Hello',
 //   name: 'TheAwesomeBot',
@@ -24,25 +18,39 @@ var botcmd = '!bot',
   helpcmd = 'help';
 
 var basicHelp = "Awesome is my name, don't wear it out! " + 
-  "Please type '!bot help *channel you need help with*' for more info.";
+  `Please type '${botcmd} ${helpcmd} *channel you need help with*' for more info.`;
 
 var helpObj = {};
 helpObj[botcmd + ' ' + helpcmd] = helpObj[botcmd] = basicHelp
 
-
+// basic help commands
 var simpleResponses = Object.assign(helpObj,
   Object.keys(helpCommands)
     .map(k => [`${botcmd} ${helpcmd} ${k}`, helpCommands[k]])
     .reduce( (obj, row) => (obj[row[0]] = row[1], obj), {}))
 
-console.log(simpleResponses)
-
-
-
 bot.on('message', function (message) {
-  var key = message.toLowerCase().trim()
+  var key = message.content.toLowerCase().trim()
   if (simpleResponses[key]) return bot.reply(message, simpleResponses[key])
 
+  if (message.content.indexOf(botcmd) !== 0) return;
+
+  var jscompileindex = message.content.indexOf('jseval')
+
+  if (jscompileindex > 0) {
+    return bot.reply(message, safer_eval(message.content.substr(jscompileindex + 7)))
+  }
+
 });
+
+function safer_eval(code) {
+  var result;
+  try {
+    result = vm.runInNewContext(code, undefined, {timeout: 100})
+  } catch (e) {
+    result = e.toString();
+  }
+  return result;
+}
 
 bot.loginWithToken('MTk4MjQ5NTI1ODU1NTE4NzIx.Cldcvw.xAQgYTI9IN_ACmCTBVydTQiM66k');
