8c3e640e527ff352e134e00c29b5600cc8c91047
fzxt <fahadahmad41@hotmail.com>
2016-07-04 20:29:47 -0400

    add new line to links

--- commands/helpText.js
+++ commands/helpText.js
@@ -1,39 +1,40 @@
 module.exports = {
-  python: 'See these links/courses for learning Python: \
-https://docs.python.org/2/ \
-https://docs.python.org/3/ \
-https://www.udacity.com/course/programming-foundations-with-python--ud036 \
-https://automatetheboringstuff.com/ \
-',
-  java: 'See these links/courses for learning Java: \
-https://docs.oracle.com/javase/7/docs/api/ \
-https://docs.oracle.com/javase/8/docs/api/ \
-https://docs.oracle.com/javase/tutorial/ \
-',
-  c_cpp: 'See these links/courses for learning C++: \
-http://www.cplusplus.com/doc/tutorial/ \
-',
-  javascript: 'See these links/courses for learning JavaScript: \
-https://www.codecademy.com/learn/javascript \
-https://www.udacity.com/course/javascript-basics--ud804 \
-https://developer.mozilla.org/en-US/docs/Web/JavaScript \
-',
-  php: 'See these links/courses for learning PHP: \
-  http://www.w3schools.com/php/ \
-  http://php.net/manual/en/tutorial.php \
-  https://www.codecademy.com/learn/php \
-  ',
-  dotnet: 'See these links/courses for learning .NET/C#: \
-https://msdn.microsoft.com/en-us/library/aa288436(v=vs.71).aspx \
-https://msdn.microsoft.com/en-us/library/xk24xdbe(v=vs.90).aspx \
-https://www.youtube.com/watch?v=Yj0G5UdBJZw&list=PLEbaEyM-xt9mVQEAXGlRRmbO2Qp_oqF-n \
-',
-  style: "Here is a link to Discord's text styles: " +
-    'https://support.discordapp.com' +
-    '/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-',
-  react: 'See these links for learning ReactJS/Flux/Redux: \
-https://facebook.github.io/react/docs/getting-started.html \
-https://www.youtube.com/watch?v=MhkGQAoc7bc&list=PLoYCgNOIyGABj2GQSlDRjgvXtqfDxKm5b ', };
+  python: `See these links/courses for learning Python: ${'\n'}
+https://docs.python.org/2/ ${'\n'}
+https://docs.python.org/3/ ${'\n'}
+https://www.udacity.com/course/programming-foundations-with-python--ud036 ${'\n'}
+https://automatetheboringstuff.com/
+`,
+  java: `See these links/courses for learning Java: ${'\n'}
+https://docs.oracle.com/javase/7/docs/api/ ${'\n'}
+https://docs.oracle.com/javase/8/docs/api/ ${'\n'}
+https://docs.oracle.com/javase/tutorial/
+`,
+  c_cpp: `See these links/courses for learning C++: ${'\n'}
+http://www.cplusplus.com/doc/tutorial/ ${'\n'}
+`,
+  javascript: `See these links/courses for learning JavaScript: ${'\n'}
+https://www.codecademy.com/learn/javascript ${'\n'}
+https://www.udacity.com/course/javascript-basics--ud804 ${'\n'}
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+`,
+  php: `See these links/courses for learning PHP: ${'\n'}
+  http://www.w3schools.com/php/ ${'\n'}
+  http://php.net/manual/en/tutorial.php ${'\n'}
+  https://www.codecademy.com/learn/php
+  `,
+  dotnet: `See these links/courses for learning .NET/C#: ${'\n'}
+https://msdn.microsoft.com/en-us/library/aa288436(v=vs.71).aspx ${'\n'}
+https://msdn.microsoft.com/en-us/library/xk24xdbe(v=vs.90).aspx ${'\n'}
+https://www.youtube.com/watch?v=Yj0G5UdBJZw&list=PLEbaEyM-xt9mVQEAXGlRRmbO2Qp_oqF-n
+`,
+
+  style: "Here is a link to Discord's text styles: " + 'https://support.discordapp.com' +
+      '/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-',
+
+  react: `See these links for learning ReactJS/Flux/Redux:
+  https://facebook.github.io/react/docs/getting-started.html ${'\n'}
+  https://www.youtube.com/watch?v=MhkGQAoc7bc&list=PLoYCgNOIyGABj2GQSlDRjgvXtqfDxKm5b`, };
 
 module.exports['.net'] = module.exports.dotnet;
 module.exports['c#'] = module.exports.dotnet;
