a014f269c114aed82a1bc53237df365082858923
rgoliveira <rgoliveira@inf.ufrgs.br>
2016-07-15 03:38:03 -0300

    code style

--- TheAwesomeBot.js
+++ TheAwesomeBot.js
@@ -1,105 +1,99 @@
-var path = require('path');
-var Discord = require('discord.js');
+const path = require('path');
+const Discord = require('discord.js');
+const Settings = require('./settings.json');
 
-function TheAwesomeBot(token, discord_opt) {
+function TheAwesomeBot(token, discordOpt) {
   this.token = token;
-  this.client = new Discord.Client(discord_opt || { autoReconnect: true });
-  this.settings = require('./settings.json');
+  this.client = new Discord.Client(discordOpt || { autoReconnect: true });
+  this.settings = Settings;
   this.commands = {};
   this.usageList = '';
 
   // store the RE as they're expensive to create
   this.cmd_re = new RegExp(`^${this.settings.bot_cmd}[\\s]+([^ ]*)[\\s]*(.*)[\\s]*`, 'i');
-};
+}
 
-TheAwesomeBot.prototype.onMessage = function () {
-  var instance = this;
-  return (function (message) {
+TheAwesomeBot.prototype.onMessage = () => {
+  const instance = this;
+  return (message => {
     // don't respond to own messages
-    if (instance.client.user.username === message.author.username)
+    if (instance.client.user.username === message.author.username) {
       return;
+    }
 
     // check if message is a command
-    var cmd_match = message.cleanContent.match(instance.cmd_re);
+    const cmdMatch = message.cleanContent.match(instance.cmd_re);
 
     // not a known command
-    if (!cmd_match || Object.keys(instance.commands).indexOf(cmd_match[1]) === -1) {
-
+    if (!cmdMatch || Object.keys(instance.commands).indexOf(cmdMatch[1]) === -1) {
       if (message.content.match(new RegExp(`^${instance.settings.bot_cmd}[\\s]*( .*)?$`, 'i'))) {
-        var helpText = 'maybe try these valid commands? *kthnxbye!*\n\n```';
+        let helpText = 'maybe try these valid commands? *kthnxbye!*\n\n```';
         helpText += instance.usageList;
         helpText += '```';
         instance.client.reply(message, helpText);
       }
-
       return;
     }
 
     // process commands
-    var cmd = cmd_match[1];
-    var cmd_args = cmd_match[2].trim();
+    const cmd = cmdMatch[1];
+    const cmdArgs = cmdMatch[2].trim();
 
-    instance.commands[cmd].run(instance, message, cmd_args);
+    instance.commands[cmd].run(instance, message, cmdArgs);
   });
 };
 
-TheAwesomeBot.prototype.onReady = function () {
-  var instance = this;
-  return (function () {
+TheAwesomeBot.prototype.onReady = () => {
+  const instance = this;
+  return (() => {
     console.log('\nConnected to discord server!');
 
     console.log('Running initializations...');
-    for (var cmd in instance.commands) {
-      if (typeof instance.commands[cmd].init == 'function') {
+    Object.keys(instance.commands).forEach(cmd => {
+      if (typeof instance.commands[cmd].init === 'function') {
         instance.commands[cmd].init(instance);
       }
-    };
+    });
   });
 };
 
-TheAwesomeBot.prototype.serverNewMember = function () {
-  var instance = this;
-  return (function (server, user) {
+TheAwesomeBot.prototype.serverNewMember = () => {
+  const instance = this;
+  return ((server, user) => {
     instance.client.sendMessage(user, instance.usageList);
   });
 };
 
-TheAwesomeBot.prototype.onDisconnected = function () {
-  return (function () {
-    console.warn('Bot has been disconnected from server...');
-  });
-};
+TheAwesomeBot.prototype.onDisconnected = () =>
+  (() => console.warn('Bot has been disconnected from server...'));
 
-TheAwesomeBot.prototype.onError = function () {
-  return (function (err) {
-    console.error('error: ', e);
-    console.error(e.trace);
+TheAwesomeBot.prototype.onError = () =>
+  ((err) => {
+    console.error('error: ', err);
+    console.error(err.trace);
   });
-};
 
-TheAwesomeBot.prototype.loadCommands = function (cmdList) {
-  var instance = this;
+TheAwesomeBot.prototype.loadCommands = (cmdList) => {
+  const instance = this;
   instance.usageList = '';
   cmdList.forEach(cmd => {
-    var fullpath = path.join(__dirname, 'commands', cmd, `${cmd}.js`);
-    var script = require(fullpath);
+    const fullpath = path.join(__dirname, 'commands', cmd, `${cmd}.js`);
+    const script = require(fullpath); // eslint-disable-line global-require
     instance.commands[cmd] = script;
 
-    var usageObj = script.usage;
-    var usageStrs = [];
+    const usageObj = script.usage;
+    const usageStrs = [];
     if (Array.isArray(usageObj)) {
       usageObj.forEach(u => usageStrs.push(u));
     } else {
       usageStrs.push(usageObj.toString());
     }
 
-    usageStrs.forEach(u => {
-      instance.usageList += `\n- ${instance.settings.bot_cmd} ${u}`
-    });
+    usageStrs.forEach(u => (instance.usageList += `\n- ${instance.settings.bot_cmd} ${u}`));
   });
 };
 
-TheAwesomeBot.prototype.init = function () {
+TheAwesomeBot.prototype.init = () => {
   // load commands
   console.log('Loading commands...');
   this.loadCommands(this.settings.commands);
