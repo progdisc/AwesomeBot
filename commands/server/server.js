7c7825cda96d8fe38e2e886698ea6447972fb321
Austin <memyselfandlie@gmail.com>
2017-02-06 16:11:00 -0500

    Ok, this better be fine

--- commands/server/server.js
+++ commands/server/server.js
@@ -3,8 +3,8 @@ const discord = require('discord.js');
 module.exports = {
   usage: 'server - prints info about the server',
   run: (bot, message) => {
-      const embed = new discord.RichEmbed();
-      embed.setTitle('Server Owner')
+    const embed = new discord.RichEmbed();
+    embed.setTitle('Server Owner')
             .setColor('#ff7260')
             .setAuthor(message.guild.name, message.guild.iconURL)
             .setDescription(message.guild.owner.user.username)
@@ -12,6 +12,6 @@ module.exports = {
             .addField('Created', message.guild.createdAt.toString(), true)
             .addField('Emojis',
             message.guild.emojis.size > 0 ? message.guild.emojis.map(d => d.toString()).join(' ') : 'None');
-      message.channel.sendEmbed(embed);
-    },
+    message.channel.sendEmbed(embed);
+  },
 };
