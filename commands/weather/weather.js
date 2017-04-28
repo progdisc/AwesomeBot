87e0c7be45f0e2b4e8edfe6912f7a9842cb478d2
Şamil NART <samil.nart@gmail.com>
2017-04-28 16:13:17 +0300

    Refactored weather command

--- commands/weather/weather.js
+++ commands/weather/weather.js
@@ -1,7 +1,8 @@
-const request = require('request');
+const request = require('request-promise');
 const discord = require('discord.js');
 
 let weatherConfig;
+let tokens;
 const geocodeEndpoint = 'https://maps.googleapis.com/maps/api/geocode/json?key=gkey&address=input';
 const darkskyEndpoint = 'https://api.darksky.net/forecast/key/lat,lng';
 
@@ -9,102 +10,128 @@ function fahrenheitToCelcius(degree) {
   return (((degree - 32) * 5) / 9).toFixed(0);
 }
 
+function getGeocodeData(address) {
+  let requestURL = geocodeEndpoint
+    .replace('gkey', tokens.google_geocode)
+    .replace('input', address);
+  requestURL = encodeURI(requestURL);
+
+  return request(requestURL).then((body) => {
+    const geocodeData = JSON.parse(body);
+
+    if (geocodeData.status !== 'OK') {
+      // eslint-disable-next-line no-throw-literal
+      throw 'I\'m sorry, the address couldn\'t be detected.';
+    }
+
+    const result = {
+      address: geocodeData.results[0].formatted_address,
+      coordinate: geocodeData.results[0].geometry.location,
+    };
+
+    return result;
+  });
+}
+
+function getWeatherData(location) {
+  const requestURL = darkskyEndpoint
+    .replace('key', tokens.darksky)
+    .replace('lat', location.coordinate.lat)
+    .replace('lng', location.coordinate.lng);
+
+  return request(requestURL).then((body) => {
+    const weatherData = JSON.parse(body);
+
+    const offset = weatherData.offset;
+    const utcTime = weatherData.currently.time;
+    // datetime is weird in javascript, please do change this part if you can
+    const localTime = new Date(utcTime * 1000);
+    localTime.setHours(localTime.getHours() + offset);
+    let dateString;
+    // toGMTString prints out timezone of host so we slice it off
+    if (offset > 0) {
+      dateString = localTime.toUTCString() + ' +' + offset;
+    } else {
+      dateString = localTime.toUTCString() + ' ' + offset;
+    }
+
+    const temperatureF = weatherData.currently.temperature.toFixed(0);
+    const temperatureC = fahrenheitToCelcius(temperatureF);
+    const summary = weatherData.currently.summary;
+    const humidity = (weatherData.currently.humidity * 100).toFixed(0);
+    // convert speed to freedom units
+    const windSpeed = (weatherData.currently.windSpeed * 1.61).toFixed(0);
+    const pressure = weatherData.currently.pressure.toFixed(0);
+    const icon = weatherData.currently.icon;
+    const timezone = weatherData.timezone;
+
+    const result = {
+      location,
+      offset,
+      localTime,
+      dateString,
+      temperatureC,
+      temperatureF,
+      summary,
+      humidity,
+      windSpeed,
+      pressure,
+      icon,
+      timezone,
+    };
+
+    return result;
+  });
+}
+
+function generateEmbed(data, verbose) {
+  const embed = new discord.RichEmbed();
+
+  if (verbose) {
+    embed.setColor('#4286f4')
+      .setFooter(`Local Time: ${data.dateString}`)
+      .setTitle(`Weather in ${data.address}`)
+      .addField('Summary', data.summary)
+      .addField('Temperature °C', `${data.temperatureC} °C`, true)
+      .addField('Temperature °F', `${data.temperatureF} °F`, true)
+      .addField('Timezone', data.timezone, true)
+      .addField('Humidity', `${data.humidity}%`, true)
+      .addField('Wind Speed', `${data.windSpeed} km/h`, true)
+      .addField('Air Pressure', `${data.pressure} mbar`, true)
+      .setDescription(weatherConfig.icons[data.icon]);
+  } else {
+    embed.setColor('#4286f4')
+      .setFooter(`Local Time: ${data.dateString}`)
+      .setTitle(`Weather in ${data.address}`)
+      .addField('Summary', data.summary)
+      .addField('Temperature', `${data.temperatureC} °C / ${data.temperatureF} °F`, true)
+      .addField('Humidity', `${data.humidity}%`, true)
+      .setDescription(weatherConfig.icons[data.icon]);
+  }
+
+  return embed;
+}
+
 module.exports = {
   usage: [
     'weather <address> - brings current weather info for given address',
     'weather -v <address> - brings additional weather info for given address',
   ],
-
-  init: (bot) => {
-    weatherConfig = bot.settings.weather;
-  },
-
   run: (bot, message, cmdArgs) => {
     if (!cmdArgs) return true;
-    let verbose = false;
+
     let args = cmdArgs.split(' ');
-    if (args[0] === '-v') {
-      verbose = true;
-      args = args.slice(1);
-    }
-    args = args.join(' ');
-    // get geocode info
-    let requestURL = geocodeEndpoint
-      .replace('gkey', weatherConfig.geocode_api_key || bot.settings.tokens.google_geocode)
-      .replace('input', args);
-    // do percentage encoding
-    requestURL = encodeURI(requestURL);
+    const verbose = args[0] === '-v';
+    args = verbose ? args.slice(1) : args;
 
-    request(requestURL, (err, res, bod) => {
-      if (err) {
-        message.chanel.sendMessage('There was a problem with geocoding request.');
-        return;
-      }
-
-      const geocodeData = JSON.parse(bod);
-
-      if (geocodeData.status !== 'OK') {
-        message.channel.sendMessage(`I'm sorry ${message.author}, your address couldn't be detected.`);
-        return;
-      }
-
-      const address = geocodeData.results[0].formatted_address;
-      const coordinate = geocodeData.results[0].geometry.location;
-      // get weather data
-      requestURL = darkskyEndpoint
-        .replace('key', weatherConfig.darksky_api_key || bot.settings.tokens.darksky)
-        .replace('lat', coordinate.lat)
-        .replace('lng', coordinate.lng);
-
-      request(requestURL, (error, response, body) => {
-        const weatherData = JSON.parse(body);
-
-        const offset = weatherData.offset;
-        const utcTime = weatherData.currently.time;
-        // datetime is weird in javascript, please do change this part if you can
-        const localTime = new Date(utcTime * 1000);
-        localTime.setHours(localTime.getHours() + offset);
-        let dateString;
-        // toGMTString prints out timezone of host so we slice it off
-        if (offset > 0) {
-          dateString = localTime.toUTCString() + ' +' + offset;
-        } else {
-          dateString = localTime.toUTCString() + ' ' + offset;
-        }
-        const temperatureF = weatherData.currently.temperature.toFixed(0);
-        const temperatureC = fahrenheitToCelcius(temperatureF);
-        const summary = weatherData.currently.summary;
-        const humidity = (weatherData.currently.humidity * 100).toFixed(0);
-        // convert speed to freedom units
-        const windSpeed = (weatherData.currently.windSpeed * 1.61).toFixed(0);
-        const pressure = weatherData.currently.pressure.toFixed(0);
-        const embed = new discord.RichEmbed();
-
-        if (verbose) {
-          embed.setColor('#4286f4')
-            .setFooter(`Local Time: ${dateString}`)
-            .setTitle(`Weather in ${address}`)
-            .addField('Summary', summary)
-            .addField('Temperature °C', `${temperatureC} °C`, true)
-            .addField('Temperature °F', `${temperatureF} °F`, true)
-            .addField('Timezone', weatherData.timezone, true)
-            .addField('Humidity', `${humidity}%`, true)
-            .addField('Wind Speed', `${windSpeed} km/h`, true)
-            .addField('Air Pressure', `${pressure} mbar`, true)
-            .setDescription(weatherConfig.icons[weatherData.currently.icon]);
-        } else {
-          embed.setColor('#4286f4')
-            .setFooter(`Local Time: ${dateString}`)
-            .setTitle(`Weather in ${address}`)
-            .addField('Summary', summary)
-            .addField('Temperature', `${temperatureC} °C / ${temperatureF} °F`, true)
-            .addField('Humidity', `${humidity}%`, true)
-            .setDescription(weatherConfig.icons[weatherData.currently.icon]);
-        }
-
-        message.channel.sendEmbed(embed);
-      });
-    });
+    getGeocodeData(args).then(getWeatherData)
+                        .then(data => generateEmbed(data, verbose))
+                        .then(embed => message.channel.sendEmbed(embed))
+                        .catch(err => message.channel.sendMessage(err.toString()));
     return false;
+  },
+  init: (bot) => {
+    weatherConfig = bot.settings.weather;
+    tokens = bot.settings.tokens;
   },
 };
