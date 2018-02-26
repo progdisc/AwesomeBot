85b58abdcda980217d87302d49cca296d4e38d28
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2018-02-26 20:48:55 +0000

    Deprecate help command (#113)
    Deprecate help command

--- commands/help/help.js
+++ commands/help/help.js
@@ -16,23 +16,13 @@ const knownTopics = {};
 
 module.exports = {
   usage: [
-    'help <topic> - displays known resources about <topic>',
-    'help - list known topics',
+    'help - links to the new resource list at https://github.com/progdisc/resources',
   ],
 
-  run: (bot, message, cmdArgs) => {
-    if (cmdArgs) {
-      const response = knownTopics[cmdArgs.toLowerCase()];
-      message.channel.sendMessage(response ||
-        'I don\'t know anything about that. If you have a suggestion, let us know!');
-    } else {
-      let r = 'Awesome is my name, don\'t wear it out! Please give a me topic for more info.';
-      r += '\n\nTopics I know something about:';
-      r += '\n```';
-      r += Object.keys(knownTopics).map(t => `\n  - ${t}`).join('');
-      r += '\n```';
-      message.channel.sendMessage(r);
-    }
+  run: (bot, message) => {
+    let r = 'This command is deprecated.\n';
+    r += 'See https://github.com/progdisc/resources for our new and improved resource list.';
+    message.channel.sendMessage(r);
   },
 
   init: () => {
